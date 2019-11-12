aa17fcd56b76b167c516cc8631c0382924dbe484
TonyBM <antoniobarreram@hotmail.com>
2019-11-12 13:55:51 -0600

    espacio entre las comas, saltos de linea en funcion

--- EjercicioFechas.rb
+++ EjercicioFechas.rb
@@ -1,14 +1,16 @@
 require 'date'
 
-def each_month(initial_date,final_date,day)
-  start_date = Date.new(initial_date.year,initial_date.mon,day)
-  end_date = Date.new(final_date.year,final_date.mon,day)
+def each_month(initial_date, final_date, day)
+  start_date = Date.new(initial_date.year, initial_date.mon, day)
+  end_date = Date.new(final_date.year, final_date.mon, day)
   array_dates =[start_date.to_s]
   aux_date = start_date.next_month
+
   while((aux_date <=> end_date) <= 0)
     aux_date = aux_date.next_month
     array_dates << aux_date.to_s
   end
+
   array_dates
 end
-print each_month(Date.new(2018,1,3),Date.new(2018,05,05),4)
+print each_month(Date.new(2018, 1, 3),Date.new(2018, 05, 05), 4)
