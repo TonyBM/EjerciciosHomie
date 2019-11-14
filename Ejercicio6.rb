602630af2c700a92b63e5403fd437ab96fc154ac
TonyBM <antoniobarreram@hotmail.com>
2019-11-14 12:02:53 -0600

    correccion espaciado

--- Ejercicio6.rb
+++ Ejercicio6.rb
@@ -1,3 +1,3 @@
 def staircase(number_stairs)
-  1.upto(number_stairs){|counter| puts ("#"*counter).rjust(number_stairs)}
+  1.upto(number_stairs){|counter| puts ("#" * counter).rjust(number_stairs)}
 end
