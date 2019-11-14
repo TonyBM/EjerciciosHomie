64fffa609cbf80dca3744868331fcc7d5f8bd820
TonyBM <antoniobarreram@hotmail.com>
2019-11-14 11:51:28 -0600

    ejercicio8

---
+++ Ejercicio_refactorizar.rb
@@ -0,0 +1,40 @@
+def get_customer_state(session_id = nil)
+  return { state: 'chat_disabled' } if !Setting.get('chat')
+  chat_session = nil
+  chat_session = Chat::Session.find_by(session_id: session_id, state: %w[waiting running]) if session_id
+
+  return reconnect_chat_session if chat_session
+  return { state: 'offline' } if active_agents?
+  waiting_count = Chat.waiting_chat_count(id)
+  return {
+      state: 'no_seats_available',
+      queue: waiting_count,
+    } if waiting_count >= max_queue
+  {state: 'online' }.unles chat   
+end
+def reconnect_chat_session(chat_session)
+  return reconnect_running_session(chat_session) if chat_session.state == "running"
+  return {
+  state: 'reconnect',
+  position: chat_session.position,
+  } if chat_session.state == 'waiting'
+end
+def reconnect_running_session(chat_session)
+    url,user = nil
+    return if chat_session.user_id
+    chat_user = User.lookup(id: chat_session.user_id)
+    url = "#{Setting.get('http_type')}://#{Setting.get('fqdn')}/api/v1/users/image/#{chat_user.image}" if chat_user.image && chat_user.image != 'none'
+    user = {
+      name:   chat_user.fullname,
+      avatar: url,
+    }
+    session = Chat::Session.messages_by_session_id(session_id)
+      return {
+            state:   'reconnect',
+            session: session,
+            agent:   user,
+        } if session
+end
+def active_agents?
+  Chat.active_agent_count([id]).zero?
+end
