d62e4a2bb710a06013791b1f7eb6184547cbbfe8
TonyBM <antoniobarreram@hotmail.com>
2019-11-15 09:35:48 -0600

    refactorizacion funcion

---
+++ Ejercicio9.rb
@@ -0,0 +1,9 @@
+
+def longname
+  name = fullname
+  return name unless organization_id
+    organization = Organization.lookup(id: organization_id)
+  return name unless organization
+    name += " (#{organization.name})"
+  name
+end
