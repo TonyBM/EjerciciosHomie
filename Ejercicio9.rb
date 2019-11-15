c211ce4c42f9c3f988d9dec9b8885dceaf273746
TonyBM <antoniobarreram@hotmail.com>
2019-11-15 09:40:42 -0600

    corrected indentation

--- Ejercicio9.rb
+++ Ejercicio9.rb
@@ -2,8 +2,8 @@
 def longname
   name = fullname
   return name unless organization_id
-    organization = Organization.lookup(id: organization_id)
+  organization = Organization.lookup(id: organization_id)
   return name unless organization
-    name += " (#{organization.name})"
+  name += " (#{organization.name})"
   name
 end
