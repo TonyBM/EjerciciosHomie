64b8794d4ffdcc4493f02758f42a6d1cd77c6a60
TonyBM <antoniobarreram@hotmail.com>
2019-11-19 11:22:50 -0600

    Ejercicio1_POO

---
+++ Ejercicio1_POO.rb
@@ -0,0 +1,13 @@
+class User
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
+
+  def full_name
+    "#{@first_name} #{@last_name}"
+  end
+end
+
+tony = User.new("Tony", "Barrera")
+puts tony.full_name
