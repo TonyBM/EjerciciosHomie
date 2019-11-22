9db591e911353698fbb794b4c3d47dcadbce3a6c
TonyBM <antoniobarreram@hotmail.com>
2019-11-22 10:33:49 -0600

    Ejercicio2

---
+++ closureSaludo.js
@@ -0,0 +1,9 @@
+function getWelcome(name){
+  return () => {return `Hi, ${name}`;}
+}
+
+const greetHomie = getWelcome('Homie');
+const greetFco = getWelcome('Fco');
+
+console.log(greetFco());
+console.log(greetHomie());
