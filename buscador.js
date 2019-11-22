407b57d13d8d30e8bfc6d5882dd89803e6566ccf
TonyBM <antoniobarreram@hotmail.com>
2019-11-22 11:40:50 -0600

    buscador

---
+++ buscador.js
@@ -0,0 +1,15 @@
+const peopleNames = ["Pedro", "Maria", "Rosa"];
+const animals = ["Caballo", "Oso", "Perro"];
+const things = ["Petroleo", "Carne", "Rosca"];
+
+function wordSearcher(term) {
+  wordsFound = []
+  const includesTerm = (currentValue) => currentValue.includes(term);
+
+  wordsFound.push(peopleNames.filter(includesTerm));
+  wordsFound.push(animals.filter(includesTerm));
+  wordsFound.push(things.filter(includesTerm));
+
+  return wordsFound.flat();
+}
+console.log(wordSearcher("ro"));
