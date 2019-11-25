2c5380b276d90dedd3fb2dcb761071d44560c78c
TonyBM <antoniobarreram@hotmail.com>
2019-11-25 10:36:29 -0600

    Ejercicio8

---
+++ ES8.js
@@ -0,0 +1,19 @@
+function sumByPoints(numbers){
+  let counter = 0;
+  let total = 0;
+
+  numbers.forEach(numero => {
+    let splitNumbers = numero.split('.');
+	numberToSum = parseInt(splitNumbers[counter]);
+
+	if(!isNaN(numberToSum))
+	{
+	  total += numberToSum;
+	  counter++;
+	}
+  });
+
+  return total;
+}
+
+console.log(sumByPoints(["13.09.2017", "13.aa.2018", "44.09.2018", "13.09.7"]));
