d67bffc3b52bdce7867e15c9a8f61380a6b59ab4
TonyBM <antoniobarreram@hotmail.com>
2019-11-25 11:31:28 -0600

    ES8

--- ES8.js
+++ ES8.js
@@ -3,8 +3,8 @@ function sumByPoints(numbers){
   let total = 0;
 
   numbers.forEach(numero => {
-  	let splitNumbers = numero.split('.');
-  	numberToSum = parseInt(splitNumbers[counter]);
+    let splitNumbers = numero.split('.');
+    numberToSum = parseInt(splitNumbers[counter]);
 
 	if(!isNaN(numberToSum)){
 	  total += numberToSum;
