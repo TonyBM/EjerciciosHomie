e84821f673eaa9d6cf3d598b09a6b938a6c21615
TonyBM <antoniobarreram@hotmail.com>
2019-11-28 09:46:12 -0600

    ruby version sum rows and columns

---
+++ sum_row_columns.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+def maximo_suma(matrix)
+  sums = []
+
+  matrix.each { |row| sums << row.sum }
+  matrix.transpose.map { |x| sums << x.reduce(:+) }
+
+  sums.max
+end
+
+a = [
+
+  [6, 7, 8],
+
+  [1, 4, 29],
+
+  [4, 78, 12]
+
+]
+p maximo_suma(a)
