60b5ad728be0317ba9f85cf8fdd5dc6a64075017
TonyBM <antoniobarreram@hotmail.com>
2019-12-02 09:04:21 -0600

    ejercicio sumas JS

--- sumRowsColumns.js
+++ sumRowsColumns.js
@@ -2,23 +2,21 @@ function max_sum_rows_collums(numbers) {
   sums = []
   transposedNumbers = numbers[0].map((col, i) => numbers.map(row => row[i]));
 
-  numbers.forEach(element => {
-    sums.push(element.reduce((previousNumber, nextNumber) => previousNumber + nextNumber));
-  });
+  
   transposedNumbers.forEach(element => {
     sums.push(element.reduce((previousNumber, nextNumber) => previousNumber + nextNumber));
   });
+  sums.concat(numbers.map((element) => {return element.reduce((previousNumber, nextNumber) => previousNumber + nextNumber)}));
+  sums.concat(transposedNumbers.map((element) => {return element.reduce((previousNumber, nextNumber) => previousNumber + nextNumber)}));
+  console.log(sums)
   maximum = sums.reduce((firstNum, secondNum) => {return Math.max(firstNum, secondNum);});
   return maximum;
 }
 
 
-numbers =
-  [
-    [6, 7, 8],
-
-    [1, 4, 29],
-
-    [4, 78, 12]
-  ]
+numbers = [
+  [6, 7, 8],
+  [1, 4, 29],
+  [4, 78, 12]
+  ];
 console.log(max_sum_rows_collums(numbers));
