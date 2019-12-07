afe76d27803aec5322b479d6a14d6c8ed5210382
TonyBM <antoniobarreram@hotmail.com>
2019-12-06 20:25:32 -0600

    version mejorada

--- count-intersections.rb
+++ count-intersections.rb
@@ -1,21 +1,20 @@
+require 'set'
 def count_interseccions(ranges)
-  counter = 0
-  number_of_ranges = ranges.count
-  2.upto(number_of_ranges) do
-    range_to_check = ranges.pop
-    ranges.each do |range|
-      counter += 1 if check_intersecctions(range, range_to_check)
-    end
+  
+  combinations = ranges.combination(2).to_a
+  count = 0
+  combinations.each do |pair|
+    count += 1 if pair_intersects?(pair)
   end
-  counter
+  count
 end
-def check_intersecctions(first_range, second_range)
-  return false if first_range[0] > second_range[1]
-
-  return false if first_range[1] < second_range[0]
-
-   true
+def pair_intersects?(pair)
+  first_range = pair[0]
+  second_range = pair[1]
+  return false if first_range[0] >= second_range[1]
+  return false if first_range[1] <= second_range[0]
+  true
 end
-puts count_interseccions([[40, 85], [10, 60], [70, 160]])
+puts count_interseccions([[40, 85], [10, 60], [60, 160]])
 puts count_interseccions([[10, 20], [5, 15], [99, 100], [16, 18]])
 puts count_interseccions([[1, 20], [5, 40], [2, 15], [6, 50], [51, 60], [6, 41]])
