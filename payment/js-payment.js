7b999e5e760e10002856b326ec0dc744306f7002
TonyBM <antoniobarreram@hotmail.com>
2019-12-13 09:49:50 -0600

    cambios al js

--- payment/js-payment.js
+++ payment/js-payment.js
@@ -3,51 +3,38 @@ let visaButton = document.getElementById("visa-image");
 let masterCardButton = document.getElementById("mastercard-image");
 let amexButton = document.getElementById("amex-image");
 
-visaButton.addEventListener("click", () => {cardSelected("visa")});
-masterCardButton.addEventListener("click", () => {cardSelected("mastercard")});
-amexButton.addEventListener("click", () => {cardSelected("amex") });
+let tarjetas = document.getElementsByClassName("card-image");
+
+for (let tarjeta of tarjetas) {
+  tarjeta.addEventListener("click", cardSelected);
+}
 
 
-function cardSelected(name){
-  let visaImage = document.getElementById(`visa-image`);
-  let mastercardImage = document.getElementById(`mastercard-image`);
-  let amexImage = document.getElementById(`amex-image`);
-  let comissionText = document.getElementById(`comission-text`);
-  let masterCardMessage = document.getElementById(`master-card-message`);
+function cardSelected() {
+  paintBorders(this);
+  masterCardMessage(this);
+  showPayButton();
+}
+function paintBorders(targetElement) {
+  for (let tarjeta of tarjetas) {
+    tarjeta.style.borderColor = "rgb(204, 204, 204)";
+  }
+  targetElement.style.borderColor = "rgb(141, 122, 255)";
+
+}
+function showPayButton() {
   let button = document.getElementById(`button-container`);
   button.style.visibility = "visible";
   button.style.display = "block";
-
-  switch(name) 
-  {
-    case "visa":
-        visaImage.style.borderColor= "rgb(141, 122, 255)";
-        mastercardImage.style.borderColor= "rgb(204, 204, 204)";
-        amexImage.style.borderColor= "rgb(204, 204, 204)";
-        comissionText.innerHTML = "* Se cobrará el 2.65% por comisión"
-        masterCardMessage.style.visibility = "hidden";
-        masterCardMessage.style.display = "none";
-      break;
-    case "mastercard":
-        mastercardImage.style.borderColor= "rgb(141, 122, 255)";
-        visaImage.style.borderColor= "rgb(204, 204, 204)";
-        amexImage.style.borderColor= "rgb(204, 204, 204)";
-        comissionText.innerHTML = "* Se cobrará el 2.65% por comisión"
-
-        masterCardMessage.style.visibility = "visible";
-        masterCardMessage.style.display = "block";
-
-      break;
-    case "amex":
-        amexImage.style.borderColor= "rgb(141, 122, 255)";
-        visaImage.style.borderColor= "rgb(204, 204, 204)";
-        mastercardImage.style.borderColor= "rgb(204, 204, 204)";
-        masterCardMessage.style.visibility = "hidden";
-        masterCardMessage.style.display = "none";
-
-        comissionText.innerHTML = "* Se cobrará el 3.65% por comisión"
-      break;
-
+}
+function masterCardMessage(targetElement) {
+  let masterCardMessageBox = document.getElementById(`master-card-message`);
+  if (targetElement.id == "mastercard-image") {
+    masterCardMessageBox.style.visibility = "visible";
+    masterCardMessageBox.style.display = "block";
   }
-  
+  else {
+    masterCardMessageBox.style.visibility = "hidden";
+    masterCardMessageBox.style.display = "none";
+  }
 }
