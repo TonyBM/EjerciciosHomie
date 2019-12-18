9aa486a0c5890c25604807b3e09bae068414b65f
TonyBM <antoniobarreram@hotmail.com>
2019-12-17 18:15:18 -0600

    Reporte

---
+++ blog/app/controllers/reports_controller.rb
@@ -0,0 +1,5 @@
+class ReportsController < ApplicationController
+  def info
+    render(:layout => "layouts/report_layout")
+  end
+end
--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'reports/info'
   get 'welcome/index'
-  get :hello_world, to:'hello_word#index'
+  get :hello_world, to: 'hello_word#index'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
