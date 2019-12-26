2e55269ac7d33042d1e2ba8e95cc7c81e202c79d
TonyBM <antoniobarreram@hotmail.com>
2019-12-26 10:53:42 -0600

    correo session creada

--- blog/app/mailers/session_creada_mailer.rb
+++ blog/app/mailers/session_creada_mailer.rb
@@ -1,8 +1,6 @@
 class SessionCreadaMailer < ApplicationMailer
   default :from => "homie@correo.com"
   def index
-    mail(to: "correo@correo.com") do |format|
-      format.html { render layout: 'mailer' }
-    end
+    mail(to: "correo@correo.com", subject: 'Se ha publicado un nuevo libro!')
   end
 end
--- blog/config/environments/development.rb
+++ blog/config/environments/development.rb
@@ -5,7 +5,13 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
+  config.action_controller.asset_host
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 
+  config.action_controller.asset_host = 'http://localhost:3000/'
+  config.action_mailer.asset_host = config.action_controller.asset_host
   # Do not eager load code on boot.
   config.eager_load = false
 
